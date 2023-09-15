5bb6fc28ee888b53e382ba0f33babd087206d32e
Michael Amaya <mikeamaya8@gmail.com>
2023-09-15 15:07:14 -0400

    Finish search functionality

--- MineGet/include/parseCommands.hh
+++ MineGet/include/parseCommands.hh
@@ -1,4 +1,5 @@
 #pragma once
+#include <nlohmann/json.hpp>
 
 // Commands that need to be parsed. Possible commands are prefixed with parse_
 bool parseHelp();
@@ -8,3 +9,10 @@ bool parseInstall(const int argc, const char** argv);
 bool parseUpdate(const int argc, const char** argv);
 bool parseReview(const int argc, const char** argv);
 bool parseSet(const int argc, const char** argv);
+
+// Helper functions
+int selectOption(int numOptions);
+
+// Gotten from https://stackoverflow.com/questions/4654636/how-to-determine-if-a-string-is-a-number-with-c
+bool isNumber(const std::string& s);
+bool installPlugin(nlohmann::json* plugin);
--- MineGet/src/parseCommands.cc
+++ MineGet/src/parseCommands.cc
@@ -18,11 +18,46 @@ bool parseSearch(const int argc, const char** argv) {
     APICaller caller{};
     std::unique_ptr<nlohmann::json> response = caller.getData(url, CallType::GET);
 
-    if (response && response->is_array() && !response->empty()) {
-        std::cout << "Found " << response->size() << "Resource(s): " << std::endl;
+    if (response && response->is_array()) {
+        std::cout << "Found " << response->size() << " Resource(s): " << std::endl;
         for (int i = 0; i < response->size(); ++i) {
-             std::cout << "(" << i << ")" << (*response)[i]["name"] << std::endl;
+            int selectionOptions = i + 1;
+            std::string name = (*response)[i]["name"];
+            std::cout << "(" << selectionOptions << ") " << name << std::endl;
         }
+
+        int selectedOption = selectOption(response->size());
+        if (selectedOption < 0) {
+            std::cout << "Invalid selection selected!" << std::endl;
+            return false;
+        }
+
+        nlohmann::json selectedItem = (*response)[selectedOption];
+        bool doneBrowsing = false;
+        while (!doneBrowsing) {
+            std::cout << "Selected " << selectedItem["name"].get<std::string>() << std::endl;
+            std::cout << "(1) Description" << std::endl;
+            std::cout << "(2) Install" << std::endl;
+            std::cout << "(3) Abort" << std::endl;
+
+            selectedOption = selectOption(3);
+            switch (selectedOption) {
+                case 0:
+                    // Description
+                    std::cout << "Description: " << selectedItem["tag"].get<std::string>() << "\n" << std::endl;
+                    break;
+                case 1:
+                    // Install
+                    // Do some stuff, then do abort stuff
+                case 2:
+                    // Abort
+                    doneBrowsing = true;
+                    break;
+                default:
+                std::cout << "Invalid selection!" << std::endl;
+            }
+        }
+
     } else {
         std::cout << "There was a fatal error searching that term!" << std::endl;
     }
@@ -30,7 +65,7 @@ bool parseSearch(const int argc, const char** argv) {
 }
 
 bool parseInstall(const int argc, const char** argv) {
-
+    
     return true;
 }
 
@@ -49,4 +84,38 @@ bool parseReview(const int argc, const char** argv) {
 
 bool parseSet(const int argc, const char** argv) {
     return true;
+}
+
+int selectOption(int numOptions) {
+    int selection = -1;
+    std::string line{};
+    
+    std::cout << std::endl;
+    std::cout << "Select one of options [1 - " << numOptions << "]: ";
+    std::getline(std::cin, line);
+    if (isNumber(line)) {
+        selection = std::stoi(line);
+    } else {
+        return -1;
+    }
+
+    if (selection < 1 || selection > numOptions) {
+        return -1;
+    }
+
+    return selection - 1;
+}
+
+bool installPlugin(nlohmann::json* plugin) {
+    bool external = (*plugin)["dd"].get<bool>();
+    if (!external) {
+
+    }
+
+    return true;
+}
+
+bool isNumber(const std::string& s){
+    return !s.empty() && std::find_if(s.begin(), 
+        s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
 }
